4c40b2ef8970983a5332153c7cc0ff24459787fe
Tiago Prestes <bptiago123@gmail.com>
2026-09-25 15:30:41 -0300

    feat: create user route

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,5 +1,6 @@
 import { UserModel, User } from "../models/user.model";
 import { CreateUserInput } from "../schema/createUserSchema";
+import { hashPassword } from "../utils/bcrypt";
 
 
 export const userRepository = {
@@ -12,7 +13,11 @@ export const userRepository = {
     },
 
     async create(data: CreateUserInput): Promise<User> {
-        const doc = await UserModel.create(data);
-        return doc.toObject() as User;
+        const passwordHash = await hashPassword(data.password)
+        const model = await UserModel.create({
+            passwordHash: passwordHash,
+            ...data
+        });
+        return model.toObject() as User;
     },
 };
